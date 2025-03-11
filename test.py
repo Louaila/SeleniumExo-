87065e68c81dacc45f9c0b61afaa73ffdabde91b
Louaila <lou591@hotmail.fr>
2025-03-11 13:34:05 +0100

    add wait explicite

--- test.py
+++ test.py
@@ -1,42 +1,44 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import NoSuchElementException
+
 """
 Définir des options pour le driver. Pour chrome, garder
 le navigateur ouvert si driver.quit() n'est pas appelée
 """
-
 options = webdriver.ChromeOptions()
 options.add_experimental_option("detach", True)
 
 #  Instancier le navigateur Chrome avec les options
 driver = webdriver.Chrome(options)
 
+# Instancier un wait explicit de 10 secondes
+wait = WebDriverWait(driver, 10)
+
 # Ouvrir en plein écran
 driver.maximize_window()
-
-
-# Wait explicite
-
-wait= WebDriverWait(driver, 10)
 
 # Naviguer vers le site de décathlon
 driver.get("https://www.decathlon.fr/")
 
 # Vérifier que le titre de la page est correct
-assert driver.title == 'DECATHLON | Magasin de Sport', "Le titre devrait être 'DECATHLON | Magasin de Sport'"
+assert driver.title == 'DECATHLON | Magasin de Sport'
 
 # Continuer sans accepter les cookies
 try:
-    wait.until(EC.element_to_be_clickable((By.CLASS_NAME, 'didomi-continue-without-agreeing'))).click()
+    wait.until(
+            EC.element_to_be_clickable((By.CLASS_NAME, "didomi-continue-without-agreeing"))
+        ).click()
 except NoSuchElementException:
     print("Cookies pop-up is not present")
 
 # Récupérer le champ de recherche en utilisant le xpath
-search_box = driver.find_element(By.XPATH, "//input[@type='search']")
+search_box = wait.until(
+        EC.element_to_be_clickable((By.XPATH, "//input[@type='search']"))
+    )
 
 # Ecrire Vélo dans le champ de recherche et simuler le bouton Entrée
 search_box.clear()
@@ -44,8 +46,10 @@ search_box.send_keys("Vélo")
 search_box.send_keys(Keys.RETURN)
 
 # Vérifier que nous sommes sur la page des vélos
-resultatRecherche = driver.find_element(By.TAG_NAME, 'h1').text
-assert resultatRecherche == "Vélos"
+resultatRecherche = wait.until(
+        EC.visibility_of_element_located((By.TAG_NAME, "h1"))
+    )
+assert resultatRecherche.text == "Vélos"
 
 #  Vérifier qu'il y a au moins un produit affiché
 product_list = driver.find_element(By.CLASS_NAME, 'product-list')
@@ -53,4 +57,3 @@ assert product_list.is_displayed(), "Products should be visible"
 
 # Fermer le navigateur
 driver.quit()
- 
